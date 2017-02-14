eb87570f26c882f4fc8bee505e5d1a77ed66c398
Fepi <fepifarina@gmail.com>
2017-02-14 20:02:04 -0300

    Corrections in the interface

--- app/Contracts/Logic/Conversation.php
+++ app/Contracts/Logic/Conversation.php
@@ -6,24 +6,24 @@ use STS\User as UserModel;
 
 interface Conversation {
 
-    public function createTripConversation($trip_id);
+    function createTripConversation($trip_id);
 
-    public function findOrCreatePrivateConversation(UserModel $user1, UserModel $user2);
+    function findOrCreatePrivateConversation(UserModel $user1, UserModel $user2);
 
-    public function getUserConversations( UserModel $user);
+    function getUserConversations( UserModel $user);
 
-    public function getConversation( UserModel $user, $conversation_id );
+    function getConversation( UserModel $user, $conversation_id );
 
-    public function getConversationByTrip ( UserModel $user, $trip_id);
+    function getConversationByTrip ( UserModel $user, $trip_id);
 
-    public function addUserToConversation( $conversationId, UserModel $user);
+    function addUserToConversation( $conversationId, UserModel $user);
 
-    public function removeUsertFromConversation( $conversationId, UserModel $user);
+    function removeUsertFromConversation( $conversationId, UserModel $user);
 
-    public function delete ( $conversationId);
+    function delete ( $conversationId);
 
-    public function send(UserModel $user, $conversationId, $message);
+    function send( UserModel $user, $conversationId, $message);
 
-    public function getMessagesFromConversation( $conversation_id, UserModel $user, $unreadMessages);
+    function getMessagesFromConversation( $conversation_id, UserModel $user, $unreadMessages);
 
 }
