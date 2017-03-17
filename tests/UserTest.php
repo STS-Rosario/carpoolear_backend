7f3b6919c08943c7f6bb6a24ab4653d4fb67769f
Fepi <fepifarina@gmail.com>
2017-03-17 10:28:45 -0300

    Merge conflict correction

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -77,12 +77,8 @@ class UserTest extends TestCase {
 
         $u1 = $userManager->update($u1, $data);
         $this->assertTrue($u1 != null);
-<<<<<<< HEAD
 
-	}
-=======
     }
->>>>>>> develop
 
     public function testActiveUser()
     {
