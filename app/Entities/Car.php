6f5ff7ced5839b2840049b9b57c01e576cb894fb
Mariano Botta <marianoabotta@gmail.com>
2017-03-17 14:25:15 -0300

    Cars trips counts

--- app/Entities/Car.php
+++ app/Entities/Car.php
@@ -8,10 +8,22 @@ class Car extends Model
 {
     protected $table = 'cars';
     protected $fillable = ['patente', 'description', 'user_id'];
-    protected $hidden = [];
+    protected $hidden = ['created_at', 'updated_at'];
+    protected $appends = ['trips_count'];
 
     public function user()
     {
         return $this->belongsTo('STS\User', 'user_id');
+    }
+
+    public function trips()
+    {
+        return $this->hasMany('STS\Entities\Trip', 'car_id');
+    }
+
+
+    public function getTripsCountAttribute()
+    {
+        return $this->trips()->count(); 
     }
 }
