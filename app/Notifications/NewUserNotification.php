ac1101db3d609f779125bedff2eb82fddf42648f
Mariano Botta <Mariano Botta>
2017-03-28 18:21:17 -0300

    Adding missing files

---
+++ app/Notifications/NewUserNotification.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace STS\Notifications;
+
+use  STS\Services\Notifications\BaseNotification;
+use  STS\Services\Notifications\Channels\DatabaseChannel;
+use  STS\Services\Notifications\Channels\MailChannel;
+
+class NewUserNotification extends BaseNotification 
+{
+    protected $via = [MailChannel::class];
+
+    public function toEmail($user)
+    {
+        return [
+            'title' => "Bienvenido a Carpoolear",
+            'email_view' => 'create_account',
+            'url' => 'http://www.carpoolear.com.ar/app#Active/' . $user->activation_token
+        ];
+    }
+}
