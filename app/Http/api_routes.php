f798febd5869085f743014248baed9a9622a1cb8
Fepi <fepifarina@gmail.com>
2017-03-31 09:42:48 -0300

    merging

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -5,20 +5,6 @@ $v1_path = 'STS\Http\Controllers\Api\v1\\';
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['middleware'=>'cors'], function ($api) use ($v1_path) {
-<<<<<<< HEAD
-    $api->post('login',     $v1_path . 'AuthController@login');
-    $api->post('retoken',   $v1_path . 'AuthController@retoken');
-    $api->post('logout',    $v1_path . 'AuthController@logout');
-    $api->post('active/{activation_token?}', $v1_path . 'AuthController@active');
-    $api->post('reset-password', $v1_path . 'AuthController@reset');
-    $api->post('change-password/{token?}', $v1_path . 'AuthController@changePasswod');
-    
-    $api->group(['prefix' => 'users'], function ($api) use ($v1_path) {
-        $api->post("/",                 $v1_path . 'UserController@create');
-        $api->get("/{id?}",             $v1_path . 'UserController@show');
-        $api->put("/",                  $v1_path . 'UserController@update');
-        $api->put("/photo",             $v1_path . 'UserController@updatePhoto');
-=======
     $api->post('login', $v1_path.'AuthController@login');
     $api->post('retoken', $v1_path.'AuthController@retoken');
     $api->post('logout', $v1_path.'AuthController@logout');
@@ -31,33 +17,18 @@ $api->version('v1', ['middleware'=>'cors'], function ($api) use ($v1_path) {
         $api->get('/{id?}', $v1_path.'UserController@show');
         $api->put('/', $v1_path.'UserController@update');
         $api->put('/photo', $v1_path.'UserController@updatePhoto');
->>>>>>> origin/fepi/feature/ConversationsApi
     });
     
     $api->group(['prefix' => 'friends'], function ($api) use ($v1_path) {
-<<<<<<< HEAD
-        $api->post("/accept/{id?}",     $v1_path . 'FriendsController@accept');
-        $api->post("/request/{id?}",    $v1_path . 'FriendsController@request');
-        $api->post("/delete/{id?}",     $v1_path . 'FriendsController@delete');
-        $api->post("/reject/{id?}",     $v1_path . 'FriendsController@reject');
-        $api->get("/",                  $v1_path . 'FriendsController@index');
-        $api->get("/pedings",           $v1_path . 'FriendsController@pedings');
-=======
         $api->post('/accept/{id?}', $v1_path.'FriendsController@accept');
         $api->post('/request/{id?}', $v1_path.'FriendsController@request');
         $api->post('/delete/{id?}', $v1_path.'FriendsController@delete');
         $api->post('/reject/{id?}', $v1_path.'FriendsController@reject');
         $api->get('/', $v1_path.'FriendsController@index');
         $api->get('/pedings', $v1_path.'FriendsController@pedings');
->>>>>>> origin/fepi/feature/ConversationsApi
     });
     
     $api->group(['prefix' => 'social'], function ($api) use ($v1_path) {
-<<<<<<< HEAD
-        $api->post("/login/{provider?}",    $v1_path . 'SocialController@login');
-        $api->post("/friends/{provider?}",  $v1_path . 'SocialController@friends');
-        $api->put("/update/{provider?}",    $v1_path . 'SocialController@update');
-=======
         $api->post('/login/{provider?}', $v1_path.'SocialController@login');
         $api->post('/friends/{provider?}', $v1_path.'SocialController@friends');
         $api->put('/update/{provider?}', $v1_path.'SocialController@update');
@@ -69,7 +40,6 @@ $api->version('v1', ['middleware'=>'cors'], function ($api) use ($v1_path) {
         $api->put('/{id?}', $v1_path.'TripController@update');
         $api->delete('/{id?}', $v1_path.'TripController@delete');
         $api->get('/{id?}', $v1_path.'TripController@show');
->>>>>>> origin/fepi/feature/ConversationsApi
     });
     
     $api->group(['prefix' => 'conversations'], function ($api) use ($v1_path) {
@@ -77,11 +47,7 @@ $api->version('v1', ['middleware'=>'cors'], function ($api) use ($v1_path) {
         $api->post("/", $v1_path . 'ConversationController@create');
         $api->get("/{id?}",  $v1_path . 'ConversationController@get_conversation');
     });
-<<<<<<< HEAD
-    
-});
-=======
-
+  
     $api->group(['prefix' => 'cars'], function ($api) use ($v1_path) {
         $api->get('/', $v1_path.'CarController@index');
         $api->post('/', $v1_path.'CarController@create');
@@ -90,4 +56,3 @@ $api->version('v1', ['middleware'=>'cors'], function ($api) use ($v1_path) {
         $api->get('/{id?}', $v1_path.'CarController@show');
     });
 });
->>>>>>> origin/fepi/feature/ConversationsApi
