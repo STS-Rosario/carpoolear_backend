a6b33826238398c735be6b19ac4b626060463493
Pablo Botta <pabloluisbotta@gmail.com>
2017-07-22 15:59:58 -0300

    Add IGNORE to SQL Insert in migration

--- database/migration.sql
+++ database/migration.sql
@@ -1,8 +1,8 @@
-alter table carpoolear5.users add old_id bigint(20) not null;
+ALTER TABLE carpoolear.users add old_id bigint(20) not null;
 
 # Query Migratoria de usuarios ----------------------------------------------------------
 # Agregar PIN!!!!
-insert into carpoolear5.users (
+INSERT IGNORE INTO carpoolear.users (
     name,
     email,  
     password,  
@@ -23,7 +23,7 @@ insert into carpoolear5.users (
     updated_at,  
     last_connection,
     old_id 
-) select  
+) SELECT
 
     CASE WHEN name IS NOT NULL THEN name ELSE '' END,
     CASE WHEN email IS NOT NULL AND email <> '' THEN 
@@ -50,24 +50,24 @@ insert into carpoolear5.users (
     updated_at,
     id
 
-from carpoolear_viejo.users; 
+FROM carpool_ear.users; 
 
 
 # Accounts de facebook ----------------------------------------------------------
-insert into carpoolear5.social_accounts (user_id, provider_user_id, provider, created_at, updated_at)
-select id, old_id, 'facebook', created_at, updated_at  from carpoolear5.users;
+INSERT IGNORE INTO carpoolear.social_accounts (user_id, provider_user_id, provider, created_at, updated_at)
+select id, old_id, 'facebook', created_at, updated_at  from carpoolear.users;
 
 # Migrations de cars -----------------------------------------------------------
-insert into carpoolear5.cars (patente, description, user_id, created_at, updated_at) 
+INSERT IGNORE INTO carpoolear.cars (patente, description, user_id, created_at, updated_at) 
 select SUBSTRING(patente, 1, 10), '', 
-    (select carpoolear5.users.id as id from carpoolear5.users where carpoolear_viejo.users.id = carpoolear5.users.old_id limit 1),
+    (select carpoolear.users.id as id from carpoolear.users where carpool_ear.users.id = carpoolear.users.old_id limit 1),
     NOW(), NOW()
-from carpoolear_viejo.users where carpoolear_viejo.users.patente is not null;
+from carpool_ear.users where carpool_ear.users.patente is not null;
 
 
 # Migrations de trips -----------------------------------------------------------
-alter table carpoolear5.trips add old_id bigint(20) not null;
-insert into carpoolear5.trips (
+alter table carpoolear.trips add old_id bigint(20) not null;
+INSERT IGNORE INTO carpoolear.trips (
   user_id,
   from_town,
   to_town, 
@@ -89,7 +89,7 @@ insert into carpoolear5.trips (
   car_id,
   old_id
 ) select 
-    (select carpoolear5.users.id as id from carpoolear5.users where carpoolear_viejo.trips.user_id = carpoolear5.users.old_id limit 1) as uid,
+    (select carpoolear.users.id as id from carpoolear.users where carpool_ear.trips.user_id = carpoolear.users.old_id limit 1) as uid,
     from_town,
     to_town,
     CASE WHEN DATE(trip_date) THEN trip_date  ELSE created_at END,
@@ -107,13 +107,13 @@ insert into carpoolear5.trips (
     CASE WHEN DATE(updated_at) THEN updated_at ELSE now() END,
     null,
     null,
-    (select carpoolear5.cars.id as car_id from carpoolear5.cars where carpoolear5.cars.user_id = uid limit 1),
+    (select carpoolear.cars.id as car_id from carpoolear.cars where carpoolear.cars.user_id = uid limit 1),
     id
-from carpoolear_viejo.trips where trip_date is not null  ;
+from carpool_ear.trips where trip_date is not null  ;
 
 
 #migrtions de passageros ------------------------------------------------
-insert into carpoolear5.trip_passengers (
+INSERT IGNORE INTO carpoolear.trip_passengers (
     user_id,
     trip_id,
     passenger_type,
@@ -122,17 +122,17 @@ insert into carpoolear5.trip_passengers (
     created_at,
     updated_at
 ) select 
-    (select carpoolear5.users.id as id from carpoolear5.users where carpoolear_viejo.trip_passengers.user_id = carpoolear5.users.old_id limit 1) as uid,
-    (select carpoolear5.trips.id as tid from carpoolear5.trips where carpoolear_viejo.trip_passengers.trip_id = carpoolear5.trips.old_id limit 1) as tid,
+    (select carpoolear.users.id as id from carpoolear.users where carpool_ear.trip_passengers.user_id = carpoolear.users.old_id limit 1) as uid,
+    (select carpoolear.trips.id as tid from carpoolear.trips where carpool_ear.trip_passengers.trip_id = carpoolear.trips.old_id limit 1) as tid,
     passenger_type,
     request_state,
     0,
     CASE WHEN DATE(created_at) THEN created_at ELSE now() END,
     CASE WHEN DATE(updated_at) THEN updated_at ELSE now() END
-from carpoolear_viejo.trip_passengers where passenger_type = 1 ;
+from carpool_ear.trip_passengers where passenger_type = 1 ;
 
 #migrtions de friends ---------------------------------
-insert into carpoolear5.friends (
+insert into carpoolear.friends (
     uid1,
     uid2,
     origin,
@@ -146,14 +146,14 @@ insert into carpoolear5.friends (
     1,
     now(),
     now()
-from carpoolear_viejo.friends 
-LEFT JOIN carpoolear5.users as u1 ON uid1 = u1.old_id
-LEFT JOIN carpoolear5.users as u2 ON uid2 = u2.old_id
+from carpool_ear.friends 
+LEFT JOIN carpoolear.users as u1 ON uid1 = u1.old_id
+LEFT JOIN carpoolear.users as u2 ON uid2 = u2.old_id
 where u1.id is not null and u2.id is not null;
 
 
 #migrtions de calificaciones ---------------------------------
-insert into carpoolear5.rating (
+INSERT IGNORE INTO carpoolear.rating (
     trip_id,
     user_id_from,
     user_id_to,
@@ -179,19 +179,19 @@ insert into carpoolear5.rating (
     '',
     null,
     1,
-    carpoolear_viejo.calificaciones.updated_at,
+    carpool_ear.calificaciones.updated_at,
     '',
-    carpoolear_viejo.calificaciones.created_at,
-    carpoolear_viejo.calificaciones.updated_at
-from carpoolear_viejo.calificaciones
-LEFT JOIN carpoolear5.trips as t ON carpoolear_viejo.calificaciones.trip_id = t.old_id 
-LEFT JOIN carpoolear5.users as u2 on to_id = u2.old_id
-LEFT JOIN carpoolear5.users as u1 on from_id = u1.old_id;
+    carpool_ear.calificaciones.created_at,
+    carpool_ear.calificaciones.updated_at
+from carpool_ear.calificaciones
+LEFT JOIN carpoolear.trips as t ON carpool_ear.calificaciones.trip_id = t.old_id 
+LEFT JOIN carpoolear.users as u2 on to_id = u2.old_id
+LEFT JOIN carpoolear.users as u1 on from_id = u1.old_id;
 
 # Migrtions de mensajes ---------------------------------
-alter table carpoolear5.conversations add old_id bigint(20) not null;
+alter table carpoolear.conversations add old_id bigint(20) not null;
 
-insert into carpoolear5.conversations (
+INSERT IGNORE INTO carpoolear.conversations (
     type,
     title,
     trip_id,
@@ -201,10 +201,10 @@ insert into carpoolear5.conversations (
     '',
     null,
     id
-from carpoolear_viejo.conversations;
+from carpool_ear.conversations;
 
 # creo los users correspondiente a la conversacion
-insert into carpoolear5.conversations_users (
+insert into carpoolear.conversations_users (
     conversation_id,
     user_id,
     `read`
@@ -212,13 +212,13 @@ insert into carpoolear5.conversations_users (
     c.id,
     u.id,
     1
-from carpoolear_viejo.conversations
-LEFT JOIN carpoolear5.users as u on user_id = u.old_id
-LEFT JOIN carpoolear5.conversations as c on carpoolear_viejo.conversations.id = c.old_id
+from carpool_ear.conversations
+LEFT JOIN carpoolear.users as u on user_id = u.old_id
+LEFT JOIN carpoolear.conversations as c on carpool_ear.conversations.id = c.old_id
 where u.id is not null;
 
 # Migrations de mensajes
-insert into carpoolear5.messages (
+INSERT IGNORE INTO carpoolear.messages (
     `text`,
     estado,
     user_id,
@@ -230,14 +230,14 @@ insert into carpoolear5.messages (
     1,
     u.id,
     c.id,
-    carpoolear_viejo.messages.created_at,
-    carpoolear_viejo.messages.updated_at
-from carpoolear_viejo.messages
-LEFT JOIN carpoolear5.users as u on user_id = u.old_id
-LEFT JOIN carpoolear5.conversations as c on conversation_id = c.old_id
+    carpool_ear.messages.created_at,
+    carpool_ear.messages.updated_at
+from carpool_ear.messages
+LEFT JOIN carpoolear.users as u on user_id = u.old_id
+LEFT JOIN carpoolear.conversations as c on conversation_id = c.old_id
 where u.id is not null;
 
-insert into carpoolear5.user_message_read (
+INSERT IGNORE INTO carpoolear.user_message_read (
     user_id,
     message_id,
     `read`,
@@ -249,10 +249,10 @@ insert into carpoolear5.user_message_read (
     1,
     now(),
     now()
-from carpoolear5.messages as m
-left join carpoolear5.conversations_users as cu on m.conversation_id = cu.conversation_id and m.user_id <> cu.user_id;
+from carpoolear.messages as m
+left join carpoolear.conversations_users as cu on m.conversation_id = cu.conversation_id and m.user_id <> cu.user_id;
 
 
-alter table carpoolear5.trips drop old_id;
-alter table carpoolear5.users drop old_id;
-alter table carpoolear5.conversations drop old_id;
+alter table carpoolear.trips drop old_id;
+alter table carpoolear.users drop old_id;
+alter table carpoolear.conversations drop old_id;
