746e5d4cae4046a26edf69817aa82713d2890f24
Fepi <fpfarina@hotmail.com>
2017-07-29 13:52:39 -0300

    correction in fiendrequestnotification

--- app/Notifications/FriendRequestNotification.php
+++ app/Notifications/FriendRequestNotification.php
@@ -17,7 +17,7 @@ class FriendRequestNotification extends BaseNotification
             'title' => 'Nueva solicitud de amistad',
             'email_view' => 'friends_request',
             'type' => 'request',
-            'url' => 'url' => config('app.url').'/app/setting/friends'
+            'url' => config('app.url').'/app/setting/friends'
         ];
     }
 
