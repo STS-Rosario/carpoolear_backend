a234bac14c2e4935709548f93be86bd7bc397963
Mariano Botta <marianoabotta@gmail.com>
2017-08-05 10:49:33 -0300

    Merge

--- app/Services/Logic/UsersManager.php
+++ app/Services/Logic/UsersManager.php
@@ -28,14 +28,14 @@ class UsersManager extends BaseManager implements UserLogic
                 'name'     => 'max:255',
                 'email'    => 'email|max:255|unique:users,email,'.$id,
                 'password' => 'min:6|confirmed',
-                'gender'   => 'string|in:Masculino,Femenino',
+                // 'gender'   => 'string|in:Masculino,Femenino,N/A',
             ]);
         } else {
             return Validator::make($data, [
                 'name'     => 'required|max:255',
                 'email'    => 'required|email|max:255|unique:users',
                 'password' => 'min:6|confirmed',
-                'gender'   => 'string|in:Masculino,Feminino',
+                // 'gender'   => 'string|in:Masculino,Feminino,N/A',
                 'emails_notifications' => 'boolean',
             ]);
         }
