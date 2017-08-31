c5e3200f1cf29dcf0d5e097cbc2bb9b2ee9fb6c5
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2017-08-30 23:35:14 -0300

    ADD: web carpoolear.com.ar
    Revisar bien que no haya faltado nada, sobre todo para que cargue home.php
    por defecto. Si falta algo avisen y modifico la PR.

---
+++ public/contacto.php
@@ -0,0 +1,33 @@
+<?php
+$page_title = 'Contacto - Carpoolear';
+$body_class = 'body-contacto'; 
+include('includes/header.php');
+?>
+        <div class="main-container">
+        	<section>
+        	    <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 pt48">
+
+                        
+                            <h2>Contacto</h2>
+                                                    
+                            <p>¿Tenés alguna duda, comentario o sugerencia? ¡Ponete en contacto con nosotros!</p>
+            
+                              
+            
+                            <h3>Facebook</h3>
+            
+                            <p>Podés contactarnos a través de nuestra página en <a href="http://facebook.com/Carpoolear" target="_blank">Facebook</a>.</p>
+            
+                            
+            
+                            <h3>Email</h3>
+            
+                            <p>Si preferís el mail, mandanos uno a <a href="carpoolear@stsrosario.org.ar">carpoolear@stsrosario.org.ar</a></p>
+
+                        </div>
+                    </div>
+        	    </div>
+            </section>
+<?php include('includes/footer.php'); ?>
---
+++ public/plataforma-recomendaciones.php
@@ -0,0 +1,48 @@
+<?php
+$page_title = 'Recomendaciones - Plataforma - Carpoolear';
+$body_class = 'body-plataforma body-recomendaciones'; 
+include('includes/header.php');
+?>
+        <div class="main-container">
+        	<section>
+        	    <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 pt48">
+                        
+                            <h2>Recomendaciones a la hora de Carpoolear</h2>
+            
+                            <p>Compartir es una gran experiencia pero debemos tener en cuenta algunas consideraciones para poder carpoolear sin inconvenientes.</p>
+            
+                            <h3>Origen y destino</h3>
+                            <p>Al crear un viaje que tenga origen y destino con direcciones precisas, nunca tenés que cargar direcciones de tu casa o similar. Siempre usar una Ciudad o un cruce de calles.</p>
+            
+                            <h3>Comunicación entre usuarios</h3>
+                            <p>Según como hayas ingresado a la plataforma, recibirás notificaciones vía correo o notificaciones vía red social cuando hayan solicitudes, aceptaciones o rechazos para carpoolear, cuando se modifique o cancele un viaje, cuando te envíen mensajes, etc. </p>
+            
+                            <h3>Carpooleando con desconocidos</h3>
+                            <p>Previo a carpoolear con usuarios de los que no tengas referencias (o sea, que no sean “amigos” o “amigos de tus amigos”), te recomendamos que te tomes unos minutos para revisar el perfil de Carpoolear de esa persona y su perfil en distintas redes sociales. De esa forma podrás contar con información acerca de la persona. También sugerimos conversar antes de viajar. Siempre es importante compartir con alguien que te genere confianza.</p>
+            
+                            <h3>Acuerdos previos al viaje</h3>
+                            <p>Acordá previamente con el conductor el recorrido del viaje, si van a compartir los costos asociados al mismo, y cuánto deberá contribuir cada uno. Recordá que el conductor no puede lucrar con el viaje, ver <strong>“Seguro Automotor y la importancia de calcular el costo del viaje”</strong> (más abajo)</p>
+                            <p>Recomendamos que el pago se realice directamente al conductor al finalizar el viaje y sin intermediarios.</p>
+            
+                            <h3>Antes de subirte a un auto o subir alguien a tu auto</h3>
+                            <ul class="lead" data-bullet="ti-control-play">
+                              <li>Chequeá los DNI para verificar si la identidad de las personas se corresponde con la que te habían dicho.</li>
+                              <li>En el momento del encuentro, chequeá que el modelo de auto y patente coincidan con el dato suministrado por el conductor.</li>
+                              <li>Pedile al conductor que te muestre su carnet de conducir, la cédula verde del vehículo y el <strong>seguro del vehículo</strong> actualizado a la fecha. El auto también debe tener la inspección técnica obligatoria hecha.</li>
+                              <li>Verificá visualmente que el auto en el que vas a viajar esté en buenas condiciones.</li>
+                              <li>Verificá que haya un cinturón de seguridad para cada pasajero</li>
+                              <li>Recordá que no estás obligado a subirte a ningún auto ni a subir a nadie a tu auto. Si las personas con las que acordaste el viaje te generan desconfianza en el momento del encuentro, si considerás alguna actitud como sospechosa, o si la documentación o el estado del auto no están en condiciones, podés manifestarlo en los mejores términos y no realizar el viaje. Si te parece que tu desconfianza o sospecha se basa en un prejuicio, podés manifestar que te surgió un imprevisto o que no te sentís bien físicamente como para viajar. También podés bajarte del auto si en algún momento te parece que el conductor maneja de forma imprudente o que no te hace sentir cómodo. Al momento de subirte al auto y durante el viaje, vos sos responsable de ejercer tu propia seguridad. Luego de pasada la fecha del viaje podés dejar una calificación negativa. </li>
+                            </ul>
+            
+                            <h3>Seguro Automotor y la importancia de calcular el costo del viaje </h3>
+                            <p>En caso de acordar compartir gastos, el costo total del viaje se calculará como la suma del <strong>costo del combustible + los peajes</strong>. El conductor podrá pedir una contribución máxima a cada pasajero del costo total del viaje dividido la cantidad de ocupantes del vehículo al momento del viaje (conductor y pasajeros). Esto debe ser respetado pues de no ser así se estaría cometiendo una actividad ilegal.</p>
+                            
+                            <p>Remarcamos que el conductor no debe obtener ganancia económica en sus viajes. Para más información, revisá los Términos y Condiciones Generales de Carpoolear.</p>
+
+                        </div>
+                    </div>
+        	    </div>
+            </section>
+<?php include('includes/footer.php'); ?>
