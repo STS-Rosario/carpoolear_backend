f25d3b53d4ca3544693740309b16e8b7191ecd44
Pablo Botta <pabloluisbotta@gmail.com>
2018-01-15 09:29:46 -0300

    Available ratings

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,4 +34,3 @@ Route::get('/autorojo', 'HomeController@autoRojo');
 Route::get('/descarga', 'HomeController@descarga');
 Route::get('/app/{name?}', 'HomeController@handleApp')->where('name', '[\/\w\.-]*');
 Route::get('/desuscribirme', 'HomeController@desuscribirme');
-
