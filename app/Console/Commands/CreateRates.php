e9e10309597af2ee23316fe4db9c0ce25525e1b7
Pablo Botta <pabloluisbotta@gmail.com>
2018-03-05 10:01:20 -0300

    Ratings create a day after

--- app/Console/Commands/CreateRates.php
+++ app/Console/Commands/CreateRates.php
@@ -42,7 +42,7 @@ class CreateRates extends Command
      */
     public function handle()
     {
-        $date = Carbon::now()->toDateTimeString();
+        $date = Carbon::now()->subDay()->toDateTimeString();
         $this->rateLogic->activeRatings($date);
     }
 }
