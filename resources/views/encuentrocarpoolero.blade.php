dba697ecfce0fe3a33af92b21ff30f4aa30247d0
Pablo Botta <pabloluisbotta@gmail.com>
2018-03-12 11:58:34 -0300

    Encuentro carpoolero

---
+++ resources/views/encuentrocarpoolero.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.master')
+
+@section('title', 'Encuentro Carpoolero - Carpoolear')
+@section('body-class', 'body-acerca acerca-de-equipo')
+
+@section('content')
+<section>
+    <style>
+        ul li {
+            margin-top: 1em;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 pt48">
+                <h2>Encuentro Carpoolero</h2>
+                <div class="row">
+                    <div class="col-sm-12">
+                            <br />
+                            <p>
+                                📍 RESERVÁ FECHA: Miércoles 14 de Marzo 📍 <br />
+                                (¡Ojo! 13 de Marzo: <b>CÓRDOBA y MENDOZA!</b>)
+                            </p>
+            
+                            <p>📣 ENCUENTRO Carpoolear + Couchsurfing simultáneo NACIONAL (esto se va a DES CON TRO LAR 😜 )</p>
+            
+                            <p>❤ ♻️  🚗 VENÍ a #compartir en comunidad esas #anécdotas de viajes que valen la pena ser contadas una y otra vez, CONOCÉ otr@s carpooler@s, INVITÁ a es@ amig@ que no sabe bien de que le estas hablando cuando le contás en qué estas... 🤔 </p>
+            
+                            <p>Vamos a ir actualizando punto y hora de encuentro de cada lugar, si querés SUMAR tu ciudad, te AYUDAMOS a coordinarlo! 😊</p>
+            
+
+                    </div>
+                    <div class="col-sm-8 col-xs-12">
+                        <ul>
+                            <li>📍 <a href="https://www.facebook.com/events/1051684498302925/">CÓRDOBA ---> 13/3 - 19hs The Temple Bar (Achaval Rodriguez 241)</a> (<a href="https://www.couchsurfing.com/events/couchsurfing-carpoolear-simultaneo-nacional-cordoba">Evento en couchsurfing)</a></li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/couchsurfing-carpoolear-simultaneo-nacional-mendoza
+                                ">MENDOZA ---> 13/3 - 19hs El palenque bar-pulpería (Aristides Villanueva 287)</a></li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/encuentro-113-cs-brc">BARILOCHE ---> 14/3 - 19.30hs</a></li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/couchsurfing-carpoolear-simultaneo-nacional-buenos-aires">BUENOS AIRES ---> 14/3 - 19hs Grasshopper BA, Gorriti 5567</a></li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/reunion-nacional-cs-carpoolear">ROSARIO ---> 14/3 - 20hs Pichangú (Salta 2597)</a> </li>
+                            <li>📍 <a href="#">SAN JUAN ---> 14/3 - 21.30hs - Praga (Avenida Libertador 3723 Oeste, antes de calle Cabaña)</a></li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/couchsurfing-carpoolear-simultaneo-nacional-parana">SANTA FE/PARANÁ ---> 14/3 - 21.15hs Sociedad Friulana (Villaguay 539, Paraná)</a> </li>
+                            <li>📍 <a href="#">SANTIAGO DEL ESTERO ---> 14/3 -</a> </li>
+                            <li>📍 <a href="https://www.couchsurfing.com/events/couchsurfing-carpoolear-simultaneo-nacional">TUCUMÁN ---> 14/3 - 21.30hs Casa Mamarracha (Santiago 109)</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                            <img src="https://instagram.fcnq2-2.fna.fbcdn.net/vp/b661315c81d6b59c9fa961029ffee901/5B3FA03B/t51.2885-15/e35/28430187_1572848106117291_8275247208714993664_n.jpg" alt="" class="pull-right" style="width: 100%;"/>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
