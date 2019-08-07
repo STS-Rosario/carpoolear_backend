bddfd205d04f10c941a89cd6dddb623ab500c0f4
Pablo Botta <pabloluisbotta@gmail.com>
2019-08-07 10:40:10 -0300

    config table

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \STS\Http\Middleware\LoadConfig::class,
         // \Barryvdh\Cors\HandleCors::class,
     ];
 
---
+++ database/migrations/2019_08_07_100352_config_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('key', 55);
+			$table->string('value', 2047);
+			$table->boolean('is_laravel');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('config');
+    }
+}
