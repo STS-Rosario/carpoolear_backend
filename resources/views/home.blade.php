56874287e85893b7a6cc51724e6f07a847ff71e8
Pablo Botta <pabloluisbotta@gmail.com>
2020-04-09 17:23:47 -0300

    platform suspended

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,30 @@
 @section('title', 'Carpoolear')
 
 @section('content')
-<section class="image-bg parallax pt240 pb104 pt-xs-80 pb-xs-80">
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 pt48">
+            <br>
+            <br>
+            <br>
+            <img src="img/suspendido.png" style="display:block;margin: 1em auto;" />
+            <br>
+            <p class="lead">
+                <strong>Carpoolear</strong> apoya las medidas preventivas por el Coronavirus y da de baja la plataforma y grupos de viaje vinculados hasta el 24 de marzo inclusive.
+            </p>
+            <p class="lead">
+                Lamentamos cualquier problema ocasionado y te pedimos no viajar si no es necesario.
+            </p>
+            <p class="lead">
+                Para más información sobre las medidas del Gobierno Argentino podés entrar a <a href="https://www.argentina.gob.ar/salud/coronavirus-COVID-19">https://www.argentina.gob.ar/salud/coronavirus-COVID-19</a>
+            </p>
+            <br>
+            <br>
+        </div>
+    </div>
+</div>
+{{-- <section class="image-bg parallax pt240 pb104 pt-xs-80 pb-xs-80">
     <div class="background-image-holder">
         <img alt="image" class="background-image" src="img/carpoolear-home3.jpg" />
     </div>
@@ -242,5 +265,5 @@
             </div>	
         </div><!--end of row-->
     </div><!--end of container-->
-</section>
+</section> --}}
 @endsection
