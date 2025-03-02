eb0b3c70f2e0547b565efdbfc9400519f08e7b24
Mariano Andres Botta <marianoabotta@gmail.com>
2025-03-02 19:12:40 -0300

    rollback

--- app/Services/Notifications/Models/ValueNotification.php
+++ app/Services/Notifications/Models/ValueNotification.php
@@ -14,8 +14,8 @@ class ValueNotification extends Model
 
     public function value()
     {
-        $this->value_type = str_replace('STS\User', 'STS\Models\User', $this->value_type);
-        $this->value_type = str_replace('Entities', 'Models', $this->value_type);
+        // $this->value_type = str_replace('STS\User', 'STS\Models\User', $this->value_type);
+        // $this->value_type = str_replace('Entities', 'Models', $this->value_type);
         if (strlen($this->value_type) > 0) {
             return $this->morphTo()->withTrashed();
         } else {
