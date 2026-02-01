22c83c7221ba2eb24ef0ef7036d57c879bc642bc
ZiadJ <ziadjkhan@gmail.com>
2026-02-02 00:26:18 +0400

    Rename migration class

--- database/migrations/2025_01_24_100000_add_weekly_schedule_to_trips_table.php
+++ database/migrations/2025_01_24_100000_add_weekly_schedule_to_trips_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserPrivateNote extends Migration
+class AddWeeklyScheduleToTripsTable extends Migration
 {
 	public function up()
 	{
