820631712d8ca6459c4737b6cb4a5e79d2ffe5cc
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-28 16:12:20 -0300

    test: add CanSendEmail listener gating coverage
    Cover email notification gating branches for opted-out users, forced notifications, and opted-in users on mail channel.

---
+++ tests/Unit/Listeners/Notification/CanSendEmailTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Listeners\Notification;
+
+use STS\Events\Notification\NotificationSending;
+use STS\Listeners\Notification\CanSendEmail;
+use STS\Services\Notifications\Channels\MailChannel;
+use Tests\TestCase;
+
+class CanSendEmailTest extends TestCase
+{
+    public function test_handle_returns_false_for_mail_channel_when_user_opted_out_and_not_forced(): void
+    {
+        $listener = new CanSendEmail;
+        $user = (object) ['emails_notifications' => false];
+        $notification = (object) ['force_email' => false];
+        $event = new NotificationSending($notification, $user, new MailChannel);
+
+        $this->assertFalse($listener->handle($event));
+    }
+
+    public function test_handle_allows_mail_when_forced_or_user_opted_in(): void
+    {
+        $listener = new CanSendEmail;
+
+        $optedOutButForced = new NotificationSending(
+            (object) ['force_email' => true],
+            (object) ['emails_notifications' => false],
+            new MailChannel
+        );
+        $this->assertNull($listener->handle($optedOutButForced));
+
+        $optedIn = new NotificationSending(
+            (object) ['force_email' => false],
+            (object) ['emails_notifications' => true],
+            new MailChannel
+        );
+        $this->assertNull($listener->handle($optedIn));
+    }
+}
