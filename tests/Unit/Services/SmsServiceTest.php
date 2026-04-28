e8bafa14604222d56c65fbcca5149a14cc8f70be
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-28 10:33:12 -0300

    test: add SmsService utility method coverage
    Add focused unit tests for verification code format and phone normalization helpers in SmsService.

---
+++ tests/Unit/Services/SmsServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use STS\Services\SmsService;
+use Tests\TestCase;
+
+class SmsServiceTest extends TestCase
+{
+    public function test_generate_verification_code_returns_six_numeric_characters(): void
+    {
+        $service = new SmsService;
+        $code = $service->generateVerificationCode();
+
+        $this->assertMatchesRegularExpression('/^\d{6}$/', $code);
+    }
+
+    public function test_format_phone_number_normalizes_argentina_numbers(): void
+    {
+        $service = new SmsService;
+
+        $this->assertSame('+541122223333', $service->formatPhoneNumber('1122223333'));
+        $this->assertSame('+541122223333', $service->formatPhoneNumber('01122223333'));
+    }
+}
