57ee462a58a2e3b2120f3bbfd528650bbac275b8
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 09:45:48 -0300

    test(notifications): pin friend request notification channels and metadata

--- tests/Unit/Notifications/FriendRequestNotificationTest.php
+++ tests/Unit/Notifications/FriendRequestNotificationTest.php
@@ -4,12 +4,31 @@ namespace Tests\Unit\Notifications;
 
 use STS\Models\User;
 use STS\Notifications\FriendRequestNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class FriendRequestNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new FriendRequestNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_returns_expected_static_payload(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $notification = new FriendRequestNotification;
 
         $email = $notification->toEmail(null);
@@ -17,7 +36,9 @@ class FriendRequestNotificationTest extends TestCase
         $this->assertSame(__('notifications.friend_request.title'), $email['title']);
         $this->assertSame('friends_request', $email['email_view']);
         $this->assertSame('request', $email['type']);
-        $this->assertSame(config('app.url').'/app/setting/friends', $email['url']);
+        $this->assertSame('https://app.test/app/setting/friends', $email['url']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
     }
 
     public function test_to_string_and_push_fallback_to_someone_when_from_is_missing(): void
@@ -31,6 +52,7 @@ class FriendRequestNotificationTest extends TestCase
         $this->assertSame($expectedMessage, $push['message']);
         $this->assertSame('/setting/friends', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_sender_id_when_available(): void
