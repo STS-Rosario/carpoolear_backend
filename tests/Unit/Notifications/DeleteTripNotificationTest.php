c53af7a9489d5d9a46171ab54b8157109ed8bf51
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 10:05:43 -0300

    test(notifications): pin delete-trip channels and payload metadata

--- tests/Unit/Notifications/DeleteTripNotificationTest.php
+++ tests/Unit/Notifications/DeleteTripNotificationTest.php
@@ -5,12 +5,31 @@ namespace Tests\Unit\Notifications;
 use STS\Models\Trip;
 use STS\Models\User;
 use STS\Notifications\DeleteTripNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class DeleteTripNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new DeleteTripNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_uses_sender_and_trip_data_when_present(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $from = User::factory()->create(['name' => 'Laura Driver']);
         $trip = Trip::factory()->create(['user_id' => $from->id]);
         $notification = new DeleteTripNotification;
@@ -21,7 +40,9 @@ class DeleteTripNotificationTest extends TestCase
 
         $this->assertSame(__('notifications.delete_trip.title', ['name' => 'Laura Driver']), $email['title']);
         $this->assertSame('delete_trip', $email['email_view']);
-        $this->assertSame(config('app.url').'/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('https://app.test/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
     }
 
     public function test_to_string_and_push_fallback_to_someone_when_from_is_missing(): void
@@ -35,6 +56,7 @@ class DeleteTripNotificationTest extends TestCase
         $this->assertSame($expectedMessage, $push['message']);
         $this->assertSame('/trips/', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_trip_id_when_available(): void
@@ -49,5 +71,6 @@ class DeleteTripNotificationTest extends TestCase
         $this->assertSame('trip', $extras['type']);
         $this->assertSame($trip->id, $extras['trip_id']);
         $this->assertSame($trip->id, $push['extras']['id']);
+        $this->assertSame('/trips/'.$trip->id, $push['url']);
     }
 }
