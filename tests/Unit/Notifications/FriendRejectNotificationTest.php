eb207d4bf794d803d304f36ec13fc724931a6fcd
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 09:55:38 -0300

    test(notifications): pin friend reject channels and metadata

--- tests/Unit/Notifications/FriendRejectNotificationTest.php
+++ tests/Unit/Notifications/FriendRejectNotificationTest.php
@@ -4,12 +4,31 @@ namespace Tests\Unit\Notifications;
 
 use STS\Models\User;
 use STS\Notifications\FriendRejectNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class FriendRejectNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new FriendRejectNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_uses_sender_data_and_reject_template_fields(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $from = User::factory()->create(['name' => 'Sofia Reject']);
         $notification = new FriendRejectNotification;
         $notification->setAttribute('from', $from);
@@ -20,6 +39,8 @@ class FriendRejectNotificationTest extends TestCase
         $this->assertSame('friends_email', $email['email_view']);
         $this->assertSame('reject', $email['type']);
         $this->assertSame(__('notifications.friend_reject.status'), $email['message_mail']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
     }
 
     public function test_to_string_and_push_fallback_to_someone_when_from_is_missing(): void
@@ -33,6 +54,7 @@ class FriendRejectNotificationTest extends TestCase
         $this->assertSame($expectedMessage, $push['message']);
         $this->assertSame('/setting/friends', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_sender_id_when_available(): void
