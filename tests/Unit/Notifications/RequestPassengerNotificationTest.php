fa86a5f9af1f6e20f736d5c2bf349e0795603de5
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 09:42:23 -0300

    test(notifications): pin request passenger channels and payload fallbacks

--- tests/Unit/Notifications/RequestPassengerNotificationTest.php
+++ tests/Unit/Notifications/RequestPassengerNotificationTest.php
@@ -5,12 +5,31 @@ namespace Tests\Unit\Notifications;
 use STS\Models\Trip;
 use STS\Models\User;
 use STS\Notifications\RequestPassengerNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class RequestPassengerNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new RequestPassengerNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_uses_sender_and_trip_when_present(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $from = User::factory()->create(['name' => 'Passenger Requester']);
         $trip = Trip::factory()->create();
         $notification = new RequestPassengerNotification;
@@ -22,7 +41,20 @@ class RequestPassengerNotificationTest extends TestCase
         $this->assertSame(__('notifications.request_passenger.title', ['name' => 'Passenger Requester']), $email['title']);
         $this->assertSame('passenger_request', $email['email_view']);
         $this->assertSame('request', $email['type']);
-        $this->assertSame(config('app.url').'/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('https://app.test/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
+    }
+
+    public function test_to_email_uses_empty_trip_suffix_when_trip_is_missing(): void
+    {
+        config(['app.url' => 'https://app.test']);
+
+        $notification = new RequestPassengerNotification;
+
+        $email = $notification->toEmail(null);
+
+        $this->assertSame('https://app.test/app/trips/', $email['url']);
     }
 
     public function test_to_string_and_push_fallback_to_someone_when_sender_is_missing(): void
@@ -36,6 +68,7 @@ class RequestPassengerNotificationTest extends TestCase
         $this->assertSame($expectedMessage, $push['message']);
         $this->assertSame('/my-trips', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_trip_id_when_available(): void
