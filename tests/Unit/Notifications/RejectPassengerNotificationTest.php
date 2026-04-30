30305f85ccb375bacbe021c4e4795d2d08151e37
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 09:48:40 -0300

    test(notifications): pin reject passenger channels and metadata

--- tests/Unit/Notifications/RejectPassengerNotificationTest.php
+++ tests/Unit/Notifications/RejectPassengerNotificationTest.php
@@ -5,12 +5,31 @@ namespace Tests\Unit\Notifications;
 use STS\Models\Trip;
 use STS\Models\User;
 use STS\Notifications\RejectPassengerNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class RejectPassengerNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new RejectPassengerNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_contains_reject_specific_fields_when_sender_and_trip_are_present(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $from = User::factory()->create(['name' => 'Driver Reject']);
         $trip = Trip::factory()->create(['user_id' => $from->id]);
         $notification = new RejectPassengerNotification;
@@ -23,7 +42,9 @@ class RejectPassengerNotificationTest extends TestCase
         $this->assertSame('passenger_email', $email['email_view']);
         $this->assertSame('reject', $email['type']);
         $this->assertSame(__('notifications.reject_passenger.status'), $email['reason_message']);
-        $this->assertSame(config('app.url').'/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('https://app.test/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
     }
 
     public function test_to_string_and_push_fallback_to_someone_when_sender_missing(): void
@@ -37,6 +58,7 @@ class RejectPassengerNotificationTest extends TestCase
         $this->assertSame($expectedMessage, $push['message']);
         $this->assertSame('/trips/', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_trip_id_when_available(): void
@@ -51,5 +73,6 @@ class RejectPassengerNotificationTest extends TestCase
         $this->assertSame('trip', $extras['type']);
         $this->assertSame($trip->id, $extras['trip_id']);
         $this->assertSame($trip->id, $push['extras']['id']);
+        $this->assertSame('/trips/'.$trip->id, $push['url']);
     }
 }
