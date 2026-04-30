07f56c54888f713b9f50b168b427c30d81f86689
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-30 09:58:39 -0300

    test(notifications): pin hour-left channels and metadata

--- tests/Unit/Notifications/HourLeftNotificationTest.php
+++ tests/Unit/Notifications/HourLeftNotificationTest.php
@@ -4,12 +4,31 @@ namespace Tests\Unit\Notifications;
 
 use STS\Models\Trip;
 use STS\Notifications\HourLeftNotification;
+use STS\Services\Notifications\Channels\DatabaseChannel;
+use STS\Services\Notifications\Channels\MailChannel;
+use STS\Services\Notifications\Channels\PushChannel;
 use Tests\TestCase;
 
 class HourLeftNotificationTest extends TestCase
 {
+    public function test_via_contains_database_mail_and_push_channels(): void
+    {
+        $notification = new HourLeftNotification;
+
+        $this->assertSame([
+            DatabaseChannel::class,
+            MailChannel::class,
+            PushChannel::class,
+        ], $notification->getVia());
+    }
+
     public function test_to_email_and_to_string_use_trip_destination_when_present(): void
     {
+        config([
+            'carpoolear.name_app' => 'Carpoolear Test',
+            'app.url' => 'https://app.test',
+        ]);
+
         $trip = Trip::factory()->create(['to_town' => 'Rosario']);
         $notification = new HourLeftNotification;
         $notification->setAttribute('trip', $trip);
@@ -19,7 +38,9 @@ class HourLeftNotificationTest extends TestCase
 
         $this->assertSame(__('notifications.hour_left.title', ['destination' => 'Rosario']), $email['title']);
         $this->assertSame('hour_left', $email['email_view']);
-        $this->assertSame(config('app.url').'/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('https://app.test/app/trips/'.$trip->id, $email['url']);
+        $this->assertSame('Carpoolear Test', $email['name_app']);
+        $this->assertSame('https://app.test', $email['domain']);
         $this->assertSame(__('notifications.hour_left.message', ['destination' => 'Rosario']), $message);
     }
 
@@ -35,6 +56,7 @@ class HourLeftNotificationTest extends TestCase
         $this->assertSame($expected, $push['message']);
         $this->assertSame('/trips/', $push['url']);
         $this->assertNull($push['extras']['id']);
+        $this->assertSame('https://carpoolear.com.ar/app/static/img/carpoolear_logo.png', $push['image']);
     }
 
     public function test_get_extras_and_push_include_trip_id_when_trip_exists(): void
@@ -49,5 +71,6 @@ class HourLeftNotificationTest extends TestCase
         $this->assertSame('trip', $extras['type']);
         $this->assertSame($trip->id, $extras['trip_id']);
         $this->assertSame($trip->id, $push['extras']['id']);
+        $this->assertSame('/trips/'.$trip->id, $push['url']);
     }
 }
