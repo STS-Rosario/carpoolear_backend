86d94525d09fa33bd1c4332ede769f158d4b2c49
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-29 21:52:54 -0300

    chore(app): bind Social contract to SocialManager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use STS\Contracts\Logic\Social;
+use STS\Services\Logic\SocialManager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(Social::class, SocialManager::class);
     }
 
     /**
