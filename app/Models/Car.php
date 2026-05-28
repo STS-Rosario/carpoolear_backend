5ce171a20ce0645763187ea186dc3e84901e46b9
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-05-28 10:42:53 -0300

    feat(cars): add soft deletes on cars table and model (green)

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,15 +4,18 @@ namespace STS\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Car extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected static function newFactory()
     {
         return \Database\Factories\CarFactory::new();
     }
+
     protected $table = 'cars';
 
     protected $fillable = ['patente', 'description', 'user_id'];
