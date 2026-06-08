b7f75a433f0e4e3c82697ff8af7016061b7d47cc
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-07 21:27:14 -0300

    remove log: create handler in TestJob listener

--- tests/Unit/Listeners/TestJobListenerTest.php
+++ tests/Unit/Listeners/TestJobListenerTest.php
@@ -2,19 +2,16 @@
 
 namespace Tests\Unit\Listeners;
 
-use Illuminate\Support\Facades\Log;
 use STS\Events\User\Create as UserCreated;
 use STS\Listeners\TestJob;
 use Tests\TestCase;
 
 class TestJobListenerTest extends TestCase
 {
-    public function test_handle_logs_when_user_create_event_is_processed(): void
+    public function test_handle_completes_without_error_when_user_create_event_is_processed(): void
     {
-        Log::spy();
-
         (new TestJob)->handle(new UserCreated(901));
 
-        Log::shouldHaveReceived('info')->with('create handler')->once();
+        $this->assertTrue(true);
     }
 }
