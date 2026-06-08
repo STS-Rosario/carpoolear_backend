6fc221807b6a43af2b18373547eb4fcd0d422361
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-07 21:27:16 -0300

    remove log: TripRepository::create seat_price_cents cap

--- app/Repository/TripRepository.php
+++ app/Repository/TripRepository.php
@@ -106,7 +106,6 @@ class TripRepository
                     $rearMaxTwoPassengers
                 );
                 if ($data['seat_price_cents'] > $maximum_seat_price_cents) {
-                    \Log::info('TripRepository::create seat_price_cents is greater than maximum_seat_price_cents, setting to maximum_seat_price_cents', [$maximum_seat_price_cents]);
                     $data['seat_price_cents'] = $maximum_seat_price_cents;
                 }
             }
