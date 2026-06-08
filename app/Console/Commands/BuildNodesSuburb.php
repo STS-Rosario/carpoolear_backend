49cbf76b0f9a6f79171a422786ff13035f2e9e0f
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-07 21:35:15 -0300

    remove log: BuildNodesSuburb county lat lng

--- app/Console/Commands/BuildNodesSuburb.php
+++ app/Console/Commands/BuildNodesSuburb.php
@@ -202,9 +202,6 @@ class BuildNodesSuburb extends Command
             return $response->address->state;
         }
         if (isset($response->address->county)) {
-            \Log::info('county');
-            \Log::info('lat'.$lat.' lng'.$long);
-
             return $response->address->county;
         }
 
