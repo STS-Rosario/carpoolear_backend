8b9d210c7c29e915c4c45d11fdf607ed906c572c
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-07 21:34:36 -0300

    remove log: ConversationsManager user id comparison

--- app/Services/Logic/ConversationsManager.php
+++ app/Services/Logic/ConversationsManager.php
@@ -265,7 +265,6 @@ class ConversationsManager extends BaseManager
                         if (count($conversation->messages) > 1) {
                             for ($i = 1; $i < count($arr); $i++) {
                                 $m = $arr[$i];
-                                \Log::info($m['user_id'].' = '.$initiator['user_id']);
                                 if ($m['user_id'] != $initiator['user_id']) {
                                     $date = Carbon::parse($initiator['created_at']);
                                     $dateLate = Carbon::parse($m['created_at']);
