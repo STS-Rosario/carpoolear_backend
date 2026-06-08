733e52667b9cbe3660d36c54469ff5d6b32fcea7
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-08 08:50:46 -0300

    test: add failing tests for Empresas link removal from static navigation

---
+++ tests/Feature/Http/StaticPageNavigationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Http;
+
+use Illuminate\Support\Facades\Config;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\TestCase;
+
+class StaticPageNavigationTest extends TestCase
+{
+    #[DataProvider('staticPageProvider')]
+    public function test_static_page_navigation_does_not_include_empresas_link(string $path): void
+    {
+        Config::set('carpoolear.home_redirection', '');
+
+        $response = $this->get($path);
+
+        $response->assertOk();
+        $response->assertDontSee('>Empresas<', false);
+        $response->assertDontSee('carpoolearmas.com.ar', false);
+    }
+
+    public static function staticPageProvider(): array
+    {
+        return [
+            'home' => ['/home'],
+            'contacto' => ['/contacto'],
+            'acerca de proyecto' => ['/acerca-de-proyecto'],
+        ];
+    }
+}
