aff147046974a39a84ebb22f4a95f477c9986fb8
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-11 09:06:59 -0300

    feat: add backup_db database connection for undo migration

--- tests/Unit/Config/BackupDatabaseConnectionTest.php
+++ tests/Unit/Config/BackupDatabaseConnectionTest.php
@@ -3,10 +3,36 @@
 namespace Tests\Unit\Config;
 
 use Illuminate\Support\Facades\DB;
+use PDO;
 use Tests\TestCase;
 
 class BackupDatabaseConnectionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->ensureBackupDatabaseExists();
+    }
+
+    private function ensureBackupDatabaseExists(): void
+    {
+        $database = config('database.connections.backup_db.database');
+        $mysql = config('database.connections.mysql');
+
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s',
+            $mysql['host'],
+            $mysql['port']
+        );
+
+        $pdo = new PDO($dsn, $mysql['username'], $mysql['password']);
+        $pdo->exec(sprintf(
+            'CREATE DATABASE IF NOT EXISTS `%s`',
+            str_replace('`', '``', $database)
+        ));
+    }
+
     public function test_backup_db_connection_is_configured(): void
     {
         $config = config('database.connections.backup_db');
