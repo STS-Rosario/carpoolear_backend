50267e6322e24ca034abfbf2da1ead7c48e555d9
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-24 18:46:37 -0300

    feat: add UserLocale helper and users.locale column

---
+++ app/Support/UserLocale.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace STS\Support;
+
+use STS\Models\User;
+
+class UserLocale
+{
+    public static function resolve(?User $user): string
+    {
+        $locale = $user?->locale;
+
+        if (is_string($locale) && $locale !== '') {
+            return $locale;
+        }
+
+        return (string) config('app.locale');
+    }
+
+    public static function withLocale(string $locale, callable $callback): mixed
+    {
+        $previous = app()->getLocale();
+        app()->setLocale($locale);
+
+        try {
+            return $callback();
+        } finally {
+            app()->setLocale($previous);
+        }
+    }
+}
